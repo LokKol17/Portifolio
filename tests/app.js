0779f1444122ef34eec130ac4bccea3e1bd8b1a4
Jorge Barbosa <jorge2007jfsb@gmail.com>
2022-07-27 22:25:18 -0300

    test / beatification

--- tests/app.js
+++ tests/app.js
@@ -13,14 +13,14 @@ function Click() {
     console.log(contador)
     if (contador == 0) {
         h1.style.cssText = 'transform: translate(0 , 25px) rotate(45deg);'
-        h2.style.width = '0px'
+        h2.style.cssText = 'width: 0px;' + 'transition: none'
         h3.style.cssText = 'transform: translate(0 , -25px) rotate(-45deg);'
     
 
         contador = 1
     } else {
         h1.style.cssText = 'transform: translate(0 , 50px) rotate(0deg);'
-        h2.style.width = '100px'
+        h2.style.cssText = 'width: 100px;' + 'transition: all .5s'
         h3.style.cssText = 'transform: translate(0 , -50px) rotate(0deg);'
 
 
