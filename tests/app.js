edbc98092a11c4d8e8a0feeb661bb744fa9a7a66
Jorge Barbosa <jorge2007jfsb@gmail.com>
2022-07-27 22:13:27 -0300

    zona de testes adicionada

---
+++ tests/app.js
@@ -0,0 +1,39 @@
+let yo = document.querySelector('#yo')
+let h1 = document.querySelector('#h1')
+let h2 = document.querySelector('#h2')
+let h3 = document.querySelector('#h3')
+
+yo.addEventListener('click', Click)
+
+let contador = 0
+
+
+function Click() {
+    console.log("YOOOOO")
+    console.log(contador)
+    if (contador == 0) {
+        h1.style.cssText = 'transform: translate(0 , 25px) rotate(45deg);'
+        h2.style.width = '0px'
+        h3.style.cssText = 'transform: translate(0 , -25px) rotate(-45deg);'
+    
+
+        contador = 1
+    } else {
+        h1.style.cssText = 'transform: translate(0 , 50px) rotate(0deg);'
+        h2.style.width = '100px'
+        h3.style.cssText = 'transform: translate(0 , -50px) rotate(0deg);'
+
+
+        contador = 0
+    }
+
+
+
+
+
+
+
+
+
+
+}
